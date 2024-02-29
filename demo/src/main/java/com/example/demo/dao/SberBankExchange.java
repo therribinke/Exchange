cedc48a2d65dd790e982b6d725871cf07bacfa01
therribinke <thekuznetsoff@yandex.ru>
2024-02-29 20:05:45 +0300

    dependency create

--- demo/src/main/java/com/example/demo/dao/SberBankExchange.java
+++ demo/src/main/java/com/example/demo/dao/SberBankExchange.java
@@ -2,7 +2,7 @@ package com.example.demo.dao;
 
 public class SberBankExchange implements Exchanger{
     @Override
-    public void getCourse(MonetaryCurrency currency1, MonetaryCurrency currency2) {
+    public void getCourse() {
 
     }
 }
